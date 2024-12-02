20db5ea1fabee8612230a20fc2ff84847977307e
ashliklao <ashliklao@gmail.com>
2024-12-01 21:19:19 -0500

    Student PUT route

--- api/backend/Student/Student_routes.py
+++ api/backend/Student/Student_routes.py
@@ -22,3 +22,22 @@ def add_new_city():
     response = make_response("Successfully added city")
     response.status_code = 200
     return response
+
+@student.route('/student', methods=['PUT'])
+def update_student_info():
+    current_app.logger.info('PUT /student route')
+
+    the_data = request.json
+    company = the_data['company']
+    city = the_data['city']
+    advisorID = the_data['advisorID']
+
+    query = '''
+        UPDATE Student SET company = %s, city = %s, rent = %s, advisorID = %s, 
+        WHERE nuID = %s
+    '''
+    data = (company, city, advisorID)
+    cursor = db.get_db().cursor()
+    r = cursor.execute(query, data)
+    db.get_db().commit()
+    return 'Student information updated'
