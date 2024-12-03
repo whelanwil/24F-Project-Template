98613aed370dbe3e4938c86a44aee36d3847011a
justiniha <justinihaime@gmail.com>
2024-12-03 17:29:05 -0500

    First attempt at connect using the adviors routes

--- app/src/pages/31_Alumn_Housing.py
+++ app/src/pages/31_Alumn_Housing.py
@@ -1,18 +1,37 @@
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 from modules.nav import SideBarLinks
+import logging
+import streamlit as st
+import requests
 
 SideBarLinks()
 
-st.write("# About this App")
+# Set up logging
+logger = logging.getLogger(__name__)
 
-st.markdown (
-    """
-    This is a demo app for CS 3200 Course Project.  
+# Title and Input for city
+st.title("Search Alumni Housing")
+city = st.text_input("Enter the city to search for alumni housing:")
 
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
+# Button to search
+if st.button('Search', use_container_width=True):
+    if city:
+        try:
+            # API call to get housing data based on city
+            response = requests.get(f'http://yourapiurl.com/alumni/{city}')
+            housing_data = response.json()
 
-    Stay tuned for more information and features to come!
-    """
-        )
+            if housing_data:
+                st.write(f"**Found {len(housing_data)} alumni offering housing in {city}:**")
+                for alum in housing_data:
+                    st.write(f"**Alumni ID:** {alum['alumID']}")
+            else:
+                st.write("No alumni found offering housing in this city.")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+    else:
+        st.warning("Please enter a city.")
+
+
+
