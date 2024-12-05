7bf94df07b6e2d676091ca76e3c33f3cdbf88260
ashliklao <ashliklao@gmail.com>
2024-12-05 15:24:18 -0500

    Modify Users Page for Admin
    Copied documentation from the other pages. Not sure if this works yet

---
+++ app/src/pages/43_modify_users.py
@@ -0,0 +1,141 @@
+import logging
+import streamlit as st
+import requests
+import pandas as pd
+from datetime import datetime
+from modules.nav import SideBarLinks
+
+# Add sidebar links
+SideBarLinks()
+
+# Set up logging
+logger = logging.getLogger(__name__)
+
+# Check the role of the user
+if "role" not in st.session_state:
+    st.error("You do not have permission to access this page.")
+else:
+    role = st.session_state["role"]
+
+    # If the user is a system administrator
+    if role in ["administrator"]:
+        st.title("Modify Users")
+        
+        # Create tabs for different actions
+        tab1, tab2, tab3 = st.tabs(["Add Alumni", "Update Alumni Information", "Add Student"])
+
+        with tab1:
+            st.subheader("Add a New Alumni to the Database")
+
+            with st.form("new_alum_form"):
+                firstName = st.text_area("First Name")
+                lastName = st.text_area("Last Name")
+                email = st.text_area("Email")
+                company = st.text_area("Current Company")
+                city = st.text_input("City")
+                major = st.text_input("Major")
+                
+                if st.form_submit_button("Add Alumni"):
+                    
+                    data = {
+                        "firstName": firstName,
+                        "lastName": lastName,
+                        "email": email,
+                        "company": company,
+                        "city": city,
+                        "major": major
+                    }
+                    
+                    response = requests.post("http://web-api:4000/administrator", json=data)
+                    if response.status_code == 200:
+                        st.success("New alumni added successfully!")
+                        st.rerun()
+                    else:
+                        st.error("Failed to add alumni. Please try again.")
+        
+        with tab2:
+            st.subheader("Update Existing Alumni Information")
+
+            api_url = f"http://web-api:4000/alumni/{al}"
+            response = requests.get(api_url)
+
+            if response.status_code == 200:
+                alumni_info = response.json()
+
+                firstName = alumni_info.get('firstName', '')
+                lastName = alumni_info.get('lastName', '')
+                email = alumni_info.get('email', '')
+                company = alumni_info.get('company', '')
+                city = alumni_info.get('city', '')
+                major = alumni_info.get('major', '')
+
+                st.subheader('Current Information:')
+                st.write(f'**First Name**: {firstName}')
+                st.write(f'**Last Name**: {lastName}')
+                st.write(f'**Email**: {email}')
+                st.write(f'**Company**: {company}')
+                st.write(f'**City**: {city}')
+                st.write(f'**Major**: {major}')
+
+                st.subheader('Fill out the following to update alumni information:')
+                with st.form('update_alum_info_form'):
+                    new_firstName = st.text_input('First Name:', value=firstName)
+                    new_lastName = st.text_input('Last Name:', value=lastName)
+                    new_email = st.text_input('Email:', value=email)
+                    new_company = st.text_input('Company:', value=company)
+                    new_city = st.text_input('City', value=city)
+                    new_major = st.text_input('Major', value=major)
+
+                    submitted = st.form_submit_button('Update Information')
+
+                    if submitted:
+                        data = {
+                            'firstName': new_firstName,
+                            'lastName': new_lastName,
+                            'email': new_email,
+                            'company': new_company,
+                            'city': new_city,
+                            'major': new_major,
+                        }
+                        
+                        update_response = requests.put(f'http://web-api:4000/student/{student_id}', json=data)
+                        if update_response.status_code == 200:
+                            st.success('Your information has been updated successfully!')
+                        else:
+                            st.error(f'Failed to update information: {update_response.text}')
+
+            else: 
+                st.error(f'Failed to fetch student information (Status Code: {response.status_code})')
+
+        with tab3:
+            st.subheader("Add a New Student to the Database")
+
+            with st.form("new_student_form"):
+                firstName = st.text_area("First Name")
+                lastName = st.text_area("Last Name")
+                email = st.text_area("Email")
+                major = st.text_area("Major")
+                company = st.text_area("Current Co-Op Company")
+                city = st.text_input("City")
+                
+                if st.form_submit_button("Add Student"):
+                    
+                    data = {
+                        "firstName": firstName,
+                        "lastName": lastName,
+                        "email": email,
+                        "major": major,
+                        "company": company,
+                        "city": city
+                    }
+                    
+                    response = requests.post("http://web-api:4000/administrator", json=data)
+                    if response.status_code == 200:
+                        st.success("New student added successfully!")
+                        st.rerun()
+                    else:
+                        st.error("Failed to add student. Please try again.")
+
+    # If the user has an unrecognized role
+    else:
+        st.error("Unrecognized role.")
