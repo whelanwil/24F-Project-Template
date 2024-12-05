fc036a95042d4f6b90bb863a79121e8c68e513f0
graceukr <graceukr@icloud.com>
2024-12-05 12:50:47 -0500

    Fixing up import / side bar

--- app/src/pages/10_Advisor_Home.py
+++ app/src/pages/10_Advisor_Home.py
@@ -1,10 +1,12 @@
 import logging
-logger = logging.getLogger(__name__)
-
 import streamlit as st
 from modules.nav import SideBarLinks
 
-st.set_page_config(layout='wide')
+# Add sidebar links
+SideBarLinks()
+
+# Set up logging
+logger = logging.getLogger(__name__)
 
 # Show appropriate sidebar links for the role of the currently logged-in user
 SideBarLinks()
