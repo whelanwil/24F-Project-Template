2de5e9df967e6309a347da0c46348dd3a83c7016
justiniha <justinihaime@gmail.com>
2024-12-06 18:25:05 -0500

    Removed the alumn-student route entirely

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -6,7 +6,7 @@ from backend.Auth.auth_routes import auth
 from backend.Alumn.Alumn_routes import alumni
 from backend.Student.Student_routes import student
 from backend.Admin.Admin_routes import admin
-from backend.Advisor.StudentAlum_routes import studentAlum
+#from backend.Advisor.StudentAlum_routes import studentAlum
 # from backend.simple.simple_routes import simple_routes
 import os
 from dotenv import load_dotenv
